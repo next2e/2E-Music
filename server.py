f4e39ac0b0405f1e757e809fd610a71cb5e05e06
Matt Farejowicz <mfarejowicz@gmail.com>
2017-02-11 02:04:26 -0500

    Fixed button and some layout

--- server.py
+++ server.py
@@ -70,4 +70,5 @@ def download(total, recvd, ratio, rate, eta):
     print(ratio)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    #app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(debug=True)
