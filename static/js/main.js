0cff548a121c17486aa28af3cb9124a5488a0c56
Matt Farejowicz <mfarejowicz@gmail.com>
2017-09-09 22:32:38 -0400

    fix focuses and can press enter to queue

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,27 @@
 $(function() {
   $("#linkButton").on("click", function(e) {
     $.post("/submit", {"video": $("#url").val()});
+    $("#url").val("");
+    $("#linkButton").blur();
     alert("Submitted! The song play/appear in the queue once it is loaded.");
   });
   $("#volup").on("click", function(e) {
+    $("#volup").blur();
     $.get("/volup");
   });
   $("#voldown").on("click", function(e) {
+    $("#voldown").blur();
     $.get("/voldown");
   });
   $("#skipButton").on("click", function(e) {
+    $("#skipButton").blur();
     $.get("/skip");
+  });
+  $('#url').keypress(function(e) {
+    if (e.keyCode == 13) {
+      $('#linkButton').click();
+      e.preventDefault();
+    }
   });
 
   setInterval(function() {
